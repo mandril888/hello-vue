5287a6154ebc1e773835bed4b5b4a1c0ffc4cd49
Arturo Rubio <arturo.rubio.89.8@gmail.com>
2017-10-01 13:28:24 +0200

    importing a component to my App

---
+++ src/components/Message.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="box">
+		<p><slot></slot></p>
+	</div>
+</template>
+
+<script>
+	export default {
+
+	}
+</script>
+
+<style lang="scss">
+	.box {
+		border-radius: 4px;
+		border: 1px solid grey;
+		margin: 20px;
+		padding: 20px;
+		p {
+			font-size: 18px;
+		}
+	}
+</style>
