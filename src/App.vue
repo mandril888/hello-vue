14f7815843cccda9a8de84d752599266c86786c8
Arturo Rubio <arturo.rubio.89.8@gmail.com>
2017-10-08 20:38:35 +0200

    getting info from films api

--- src/App.vue
+++ src/App.vue
@@ -25,27 +25,30 @@
 			</section>
 
 			<section class="section">
-				<div class="field">
-					<label class="label">Name</label>
-					<div class="control has-icons-left has-icons-right">
-						<input class="input" type="text" placeholder="Text input" value="">
-						<span class="icon is-small is-left">
-							<i class="fa fa-user"></i>
-						</span>
-						<span class="icon is-small is-right">
-							<i class="fa fa-check"></i>
-						</span>
+				<form action="POST" @submit.prevent="onSubmit">
+					<div class="field">
+						<label class="label">Name</label>
+						<div class="control has-icons-left has-icons-right">
+							<input class="input" type="text" placeholder="Text input" v-model="name">
+							<span class="icon is-small is-left">
+								<i class="fa fa-user"></i>
+							</span>
+							<span class="icon is-small is-right">
+								<i class="fa fa-check"></i>
+							</span>
+						</div>
 					</div>
-				</div>
-				<div class="field">
-					<label class="label">Message</label>
-					<div class="control">
-						<textarea class="textarea" placeholder="Textarea"></textarea>
+					<div class="field is-grouped">
+						<div class="control">
+							<button class="button is-primary">Submit</button>
+						</div>
 					</div>
-				</div>
-				<div class="field is-grouped">
-					<div class="control">
-						<button class="button is-primary">Submit</button>
+				</form>
+				<div class="films" v-for="film in filmsSearched">
+					<img :src="'http://image.tmdb.org/t/p/w500' + film.poster_path"></img>
+					<div>
+						<h2>{{ film.title }}</h2>
+						<p>{{ film.overview }}</p>
 					</div>
 				</div>
 			</section>
@@ -68,12 +71,17 @@ export default {
 	data () {
 		return {
 			msg: 'Welcome to Your Vue.js App',
-			infoGet: ''
+			infoGet: '',
+			name: '',
+			filmsSearched: ''
 		}
 	},
 	methods: {
 		getInfo() {
 			axios.get('https://itunes.apple.com/search?term=jack+johnson').then(response => this.infoGet = response.data.results)
+		},
+		onSubmit() {
+			axios.get('https://api.themoviedb.org/3/search/movie?api_key=71bd8c83c5cc06c197435d2165ac52e4&query=' + this.name + '&language=es').then(response => this.filmsSearched = response.data.results)
 		}
 	}
 }
@@ -82,5 +90,25 @@ export default {
 <style lang="sass" src="bulma"></style>
 
 <style lang="scss">
-
+	.films {
+		border: 1px solid grey;
+		margin: 15px 0;
+		padding: 15px;
+		display: flex;
+		img {
+			width: 100px;
+			float: left;
+			margin: 0 10px 0 0;
+			height: 150px;
+		}
+		div {
+			clear: both;
+			h2 {
+				font-size: 20px;
+				font-weight: bolder;
+				color: #00d1b2;
+				text-shadow: 0.5px 0.5px black;
+			}
+		}
+	}
 </style>
