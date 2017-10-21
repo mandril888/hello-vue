1a2fc27bc731a35ba09ae9fc455ed01faab35928
Arturo Rubio <arturo.rubio.89.8@gmail.com>
2017-10-21 20:42:42 +0200

    insert auth

---
+++ src/service/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyDb8MnCZ6ASHDCZJKPuoPcT9gkcg0thEj4",
+	authDomain: "vuejs-9ae6b.firebaseapp.com",
+	databaseURL: "https://vuejs-9ae6b.firebaseio.com",
+	projectId: "vuejs-9ae6b",
+	storageBucket: "vuejs-9ae6b.appspot.com",
+	messagingSenderId: "875548280462"
+};
+firebase.initializeApp(config);
+
+export default {
+ database: firebase.database()
+}
